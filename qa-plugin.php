327784f2b51a61cd8806f15fca5fc25015930919
Knowledge Buzz <85382558+knowledgebuzz@users.noreply.github.com>
2026-03-05 08:46:09 +0300

    added qa plugin

---
+++ qa-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Title First Addon for MayroPro-master
+Plugin URI: https://unitedafrica.digital/
+Plugin Description: Displays question title before author in list/archive pages for MayroPro-master theme.
+Plugin Version: 1.0
+Plugin Date: 2026-03-05
+Plugin Author: Davis Simiyu Wanyonyi
+Plugin Author URI: https://unitedafrica.digital/davis
+Plugin GitHub: https://github.com/knowledgebuzz
+*/
+qa_register_plugin_layer('qa-title-first-layer.php', 'Title First Addon for MayroPro-master');
